0eaf720811cda6adfdb80774c6ccd952d34e784e
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-04-27 17:16:26 -0400

    Time: 64 ms (28.88%), Space: 13.9 MB (69.33%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,43 +1,32 @@
-class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific_reachable = set()
-        atlantic_reachable = set()
-
+class Solution(object):
+    def pacificAtlantic(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        pacific_visit = set()
+        atlantic_visit = set()
+        
         def dfs(r, c, visited):
             visited.add((r, c))
-            directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
+            directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
             for dr, dc in directions:
-                new_r, new_c = r + dr, c + dc
-
-                if (
-                    0 <= new_r < len(heights) and
-                    0 <= new_c < len(heights[0]) and
-                    (new_r, new_c) not in visited and
-                    heights[new_r][new_c] >= heights[r][c]
-                ):
-                    dfs(new_r, new_c, visited)
-        # Top row (Pacific)
-        for c in range(len(heights[0])):
-            dfs(0, c, pacific_reachable)
-
-        # Left column (Pacific)
-        for r in range(len(heights)):
-            dfs(r, 0, pacific_reachable)
-
-        # Bottom row (Atlantic)
-        for c in range(len(heights[0])):
-            dfs(len(heights)-1, c, atlantic_reachable)
-
-        # Right column (Atlantic)
-        for r in range(len(heights)):
-            dfs(r, len(heights[0])-1, atlantic_reachable)
+                row, col = r + dr, c + dc
+                if (0 <= row < len(heights) and 0 <= col < len(heights[0]) and (row, col) not in visited and heights[r][c] <= heights[row][col]):
+                    dfs(row, col, visited)
         
-        result = []
-        for r in range(len(heights)):
+        for i in range(len(heights[0])):
+            dfs(0, i, pacific_visit)
+            dfs(len(heights) - 1, i, atlantic_visit)
+
+        for i in range(len(heights)):
+            dfs(i, 0, pacific_visit)
+            dfs(i, len(heights[0]) - 1, atlantic_visit)
+        res = []
+        for r  in range(len(heights)):
             for c in range(len(heights[0])):
-                if (r, c) in pacific_reachable and (r, c) in atlantic_reachable:
-                    result.append([r, c])
-        return result
+                if (r, c) in pacific_visit and (r, c) in atlantic_visit:
+                    res.append((r, c))
+        return res
 
-
+        
