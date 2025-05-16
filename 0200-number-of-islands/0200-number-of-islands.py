6773b17fe3d44d8b0e472f223bb2866991edd4ad
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-05-16 12:28:12 -0400

    Time: 1648 ms (5.11%), Space: 31.5 MB (12.15%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,28 +6,26 @@ class Solution(object):
         """
         if not grid:
             return 0
-        rows, cols = len(grid), len(grid[0])
+
         visited = set()
+        ROWS, COLS = len(grid), len(grid[0])
         islands = 0
 
         def bfs(r, c):
             q = deque()
             q.append((r, c))
             visited.add((r, c))
-
             while q:
                 row, col = q.popleft()
-                directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+                directions =[[-1, 0], [1, 0], [0, 1], [0, -1]]
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if ((r) in range(rows) and (c) in range(cols) and grid[r][c] == "1" and (r, c) not in visited):
-                        q.append((r, c))
+                    if((r) in range(ROWS) and (c) in range(COLS) and grid[r][c] == "1" and (r, c) not in visited):
                         visited.add((r, c))
-
-        for r in range(rows):
-            for c in range(cols):
+                        q.append((r, c))
+        for r in range(ROWS):
+            for c in range(COLS):
                 if (r, c) not in visited and grid[r][c] == "1":
                     bfs(r, c)
-                    islands += 1
+                    islands +=1
         return islands
-        
