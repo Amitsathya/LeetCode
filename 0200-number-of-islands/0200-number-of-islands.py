5caaf19e72f21f65b86b35bde6c11edc3162fac1
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-06-13 23:00:24 -0400

    Time: 342 ms (5.12%), Space: 25.5 MB (21.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,11 +1,6 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
-        if not grid:
-            return 0
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
 
         visited = set()
         ROWS, COLS = len(grid), len(grid[0])
@@ -15,17 +10,19 @@ class Solution(object):
             q = deque()
             q.append((r, c))
             visited.add((r, c))
+
             while q:
                 row, col = q.popleft()
-                directions =[[-1, 0], [1, 0], [0, 1], [0, -1]]
+                directions =[[-1, 0 ], [1, 0], [0, 1], [0, -1]]
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if((r) in range(ROWS) and (c) in range(COLS) and grid[r][c] == "1" and (r, c) not in visited):
-                        visited.add((r, c))
+                    if(c in range(COLS) and r in range(ROWS) and (r, c) not in visited and grid[r][c] == "1" ):
                         q.append((r, c))
+                        visited.add((r, c))
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) not in visited and grid[r][c] == "1":
                     bfs(r, c)
-                    islands +=1
+                    islands += 1
         return islands
