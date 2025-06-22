d9a8a98c6a47845ec976fbedd0b34f8c6aa41cac
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-06-22 19:12:04 -0400

    Time: 47 ms (25.86%), Space: 19.4 MB (46.59%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,31 +1,28 @@
-class Solution(object):
-    def pacificAtlantic(self, heights):
-        """
-        :type heights: List[List[int]]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         atlantic_visited = set()
         pacific_visited = set()
-        rows, cols = len(heights), len(heights[0])
+        ROWS, COLS = len(heights), len(heights[0])
+
         def dfs(r, c, visited):
             visited.add((r, c))
-            directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
-            
+            directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+
             for dr, dc in directions:
                 row, col = r + dr, c + dc
-                if (0 <= row < rows and 0 <= col < cols and (row, col) not in visited and heights[row][col] >= heights[r][c]):
+                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in visited and heights[r][c] <= heights[row][col]:
                     dfs(row, col, visited)
         
-        for i in range(cols):
-            dfs(0, i, pacific_visited)
-            dfs(rows - 1, i, atlantic_visited)
+        for i in range(ROWS):
+            dfs(i, 0, pacific_visited)
+            dfs(i, COLS - 1, atlantic_visited)
         
-        for j in range(rows):
-            dfs(j, 0, pacific_visited)
-            dfs(j, cols - 1, atlantic_visited)
+        for j in range(COLS):
+            dfs(0, j, pacific_visited)
+            dfs(ROWS - 1, j, atlantic_visited)
         res = []
-        for r in range(rows):
-            for c in range(cols):
-                if (r, c) in pacific_visited and (r, c) in atlantic_visited:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in atlantic_visited and (r, c) in pacific_visited:
                     res.append((r, c))
         return res
