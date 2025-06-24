126e0df2f9fc3a33e05df112602f12b35954ac1c
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-06-24 17:37:40 -0400

    Time: 275 ms (26.67%), Space: 25.5 MB (17.7%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,10 +1,8 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid: return 0
-
         ROWS, COLS = len(grid), len(grid[0])
-        islands = 0
         visited = set()
+        islands = 0
 
         def dfs(r, c):
             queue = deque()
@@ -12,8 +10,8 @@ class Solution:
             visited.add((r, c))
 
             while queue:
-                row, col = queue.popleft()
                 directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+                row, col = queue.popleft()
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
                     if 0 <= r < ROWS and 0 <= c < COLS and (r, c) not in visited and grid[r][c] == "1":
@@ -25,4 +23,3 @@ class Solution:
                     dfs(r, c)
                     islands += 1
         return islands
-        
