9e4a1bb268d0d1d0338520a4492aa0330db2b15b
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-07-03 15:44:32 -0400

    Time: 8 ms (23.9%), Space: 20 MB (8.19%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -6,21 +6,20 @@ class Solution:
 
         for course, pre in prerequisites:
             graph[course].append(pre)
-
+        
         def dfs(course):
             if course in visited:
                 return False
             if course in finished:
                 return True
             visited.add(course)
-            for pre in graph[course]:
-                if not dfs(pre):
+            for nei in graph[course]:
+                if not dfs(nei):
                     return False
             visited.remove(course)
             finished.add(course)
             return True
-
-        for i in range(numCourses):
-            if not dfs(i):
+        for n in range(numCourses):
+            if not dfs(n):
                 return False
-        return True        
+        return True
