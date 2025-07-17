e1d40f035f9858758ecc4ff11d97ffd972cad2d4
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-07-17 00:16:33 -0400

    Time: 5712 ms (16.05%), Space: 17.8 MB (64.7%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -4,23 +4,22 @@ class Solution:
         path = set()
 
         def dfs(r, c, i):
-            if  i == len(word):
+            if i == len(word):
                 return True
-            if (r < 0 or c < 0
-            or r >= ROWS or c >= COLS
-            or board[r][c] != word[i]
-            or (r, c) in path):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or board[r][c] != word[i] or (r, c) in path:
                 return False
-
+            
             path.add((r, c))
-            res = (dfs(r + 1, c, i + 1) or 
-                dfs(r - 1, c, i + 1)  or
-                dfs(r, c + 1, i + 1) or
-                dfs(r, c - 1, i + 1))
+            directions =[[-1, 0], [0, -1], [1, 0], [0, 1]]
+            for dr, dc in directions:
+                row, col = r + dr, c + dc
+                if dfs(row, col, i + 1):
+                    return True
             path.remove((r, c))
-            return res
-
+            return False
+        
         for r in range(ROWS):
             for c in range(COLS):
-                if dfs(r, c, 0): return True
-        return False 
+                if dfs(r, c, 0):
+                    return True
+        return False
