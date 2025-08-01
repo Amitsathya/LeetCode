18ec293c4bf205b4f8f8bdabd6f8b6896c240409
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-01 09:10:37 -0400

    Time: 261 ms (39.22%), Space: 25.5 MB (17.82%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,21 +5,21 @@ class Solution:
         visited = set()
 
         def dfs(r, c):
+            visited.add((r, c))
             queue = deque()
             queue.append((r, c))
-            visited.add((r, c))
+
             while queue:
                 row, col = queue.popleft()
                 directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if 0 <= r < ROWS and 0 <= c < COLS and (r, c) not in visited and grid[r][c] == "1":
+                    if r >= 0 and r < ROWS and c >=0 and c < COLS and grid[r][c] == "1" and (r, c) not in visited:
                         queue.append((r, c))
                         visited.add((r, c))
-        
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) not in visited and grid[r][c] == "1":
+                if (r, c)not in visited and grid[r][c] == "1":
                     dfs(r, c)
                     islands += 1
         return islands
