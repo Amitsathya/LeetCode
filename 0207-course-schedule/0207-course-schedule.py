7357692ce4c39c33a3bb3c7d87a89a19ee9dd55b
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-01 09:45:05 -0400

    Time: 6 ms (51.45%), Space: 19.9 MB (10.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,12 +1,10 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visited = set()
+        visited  = set()
         finished = set()
-        graph = defaultdict(list)
-
+        graph = {i:[] for i in range(numCourses)}
         for course, pre in prerequisites:
             graph[course].append(pre)
-        
         def dfs(course):
             if course in visited:
                 return False
