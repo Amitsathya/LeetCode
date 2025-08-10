eb5bc37690cb49253909e95dee418b2ace6f15ce
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-10 19:20:03 -0400

    Time: 4 ms (42.2%), Space: 18.9 MB (32.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dicts = {}
+        mapped = {}
         for i, n in enumerate(nums):
             diff = target - n
-            if diff in dicts:
-                return [i, dicts[diff]]
-            dicts[n] = i
+            if diff in mapped:
+                return [i, mapped[diff]]
+            mapped[n] = i
         return -1
-        
