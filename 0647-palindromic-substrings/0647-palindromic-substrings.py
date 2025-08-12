681df4288981a322edff2e9af78b1e49579c5c29
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-12 12:41:36 -0400

    Time: 133 ms (50.47%), Space: 17.7 MB (87.68%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,15 +1,13 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
-
-        def dfs(l, r):
-            temp = 0
+        def checkIfValid(l, r):
+            length = 0
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                temp += 1
                 l, r = l - 1, r + 1
-            return temp
-        
+                length += 1
+            return length
         for i in range(len(s)):
-            res += dfs(i, i)
-            res += dfs(i, i + 1)
+            res += checkIfValid(i, i)
+            res += checkIfValid(i, i + 1)
         return res
