787a11e476a84baf17e5aca2981454942eccc4d1
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-13 20:59:27 -0400

    Time: 411 ms (95.05%), Space: 20.6 MB (69.59%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,19 +5,20 @@ class Solution:
         for i, v in enumerate(nums):
             if v > 0:
                 break
+            
             if i > 0 and nums[i - 1] == v:
                 continue
+            
             l, r = i + 1, len(nums) - 1
             while l < r:
-                temp = v + nums[l] + nums[r]
-                if temp > 0:
+                sum3 = v + nums[l] + nums[r]
+                if sum3 > 0:
                     r -= 1
-                elif temp < 0:
+                elif sum3 < 0:
                     l += 1
                 else:
                     res.append([v, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while l < r and nums[l] == nums[l - 1]:
+                    l, r = l + 1, r - 1
+                    while l < r and nums[l - 1] == nums[l]:
                         l += 1
         return res
