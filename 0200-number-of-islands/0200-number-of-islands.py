b066691af80f79ec983bdbfeab7791de9b30cbf5
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-16 12:44:54 -0400

    Time: 333 ms (5.04%), Space: 32.6 MB (5.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
         islands = 0
-        ROWS, COLS = len(grid), len(grid[0])
         visited = set()
+        ROWS, COLS = len(grid), len(grid[0])
 
         def dfs(r, c):
-            visited.add((r, c))
             queue = deque()
             queue.append((r, c))
+            visited.add((r, c))
 
             while queue:
-                row, col = queue.popleft()
+                r, c = queue.popleft()
                 directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
                 for dr, dc in directions:
-                    r, c = row + dr, col + dc
-                    if r >= 0 and r < ROWS and c >=0 and c < COLS and grid[r][c] == "1" and (r, c) not in visited:
-                        queue.append((r, c))
-                        visited.add((r, c))
+                    nr, nc = dr + r, dc + c
+                    if 0 <= nr < ROWS and 0 <= nc < COLS and (nr, nc) not in visited and grid[r][c] == "1":
+                        queue.append((nr, nc))
+                        visited.add((nr, nc))
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c)not in visited and grid[r][c] == "1":
+                if (r, c) not in visited and grid[r][c] == "1":
                     dfs(r, c)
                     islands += 1
         return islands
