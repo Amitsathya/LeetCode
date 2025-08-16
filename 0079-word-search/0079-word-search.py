0297c136645b144be3b66276b6357040735be249
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-08-16 11:55:17 -0400

    Time: 5761 ms (15.33%), Space: 18 MB (39.75%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,22 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS= len(board), len(board[0])
+        ROWS, COLS = len(board), len(board[0])
         path = set()
 
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            if r < 0 or r >= ROWS or c < 0 or c >= COLS or (r, c) in path or board[r][c] != word[i]:
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or board[r][c] != word[i] or (r, c) in path:
                 return False
             
             path.add((r, c))
             directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
             for dr, dc in directions:
-                row, col = r + dr, c + dc
-                if dfs(row, col, i + 1):
+                nr, nc = dr + r, dc + c
+                if dfs(nr, nc, i + 1):
                     return True
             path.remove((r, c))
             return False
+        
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(r, c, 0):
