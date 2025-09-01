47882cd66f016b0d5b31782623a4b737b8661d7c
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-01 12:18:25 -0400

    Time: 47 ms (26.13%), Space: 19.4 MB (46.1%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,23 +1,24 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pacific_visited, atlantic_visited = set(), set()
-        res = []
+        atlantic_visited, pacific_visited = set(), set()
 
         def dfs(r, c, visited):
             visited.add((r, c))
             directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
             for dr, dc in directions:
                 row, col = r + dr, c + dc
-                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in visited and  heights[r][c] <= heights[row][col]:
+                if 0 <= row < ROWS and 0 <= col < COLS and heights[row][col] >= heights[r][c] and (row, col) not in visited:
                     dfs(row, col, visited)
+        
         for i in range(ROWS):
             dfs(i, 0, pacific_visited)
             dfs(i, COLS - 1, atlantic_visited)
+        
         for j in range(COLS):
             dfs(0, j, pacific_visited)
             dfs(ROWS - 1, j, atlantic_visited)
-
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
                 if (r, c) in atlantic_visited and (r, c) in pacific_visited:
