3725bc5c3cc2a1483eebd0c03ee2962fe2f85848
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-04 12:30:01 -0400

    Time: 55 ms (91.94%), Space: 18.2 MB (77.31%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,27 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if len(t) == 0: return ""
         countT, window = {}, {}
-        for c in t:
-            countT[c] = countT.get(c, 0) + 1
+        for i in range(len(t)):
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        l, resLen, res = 0, float('inf'), [-1, -1]
         have, need = 0, len(countT)
-        res, resLen, l = [-1, -1], float('inf'), 0
         for r in range(len(s)):
             c = s[r]
             window[c] = window.get(c, 0) + 1
-            if c in countT and window[c] == countT[c]:
+            if c in countT and countT[c] == window[c]:
                 have += 1
-            
             while have == need:
-                if resLen > (r - l + 1):
+                c = s[l]
+                if r - l + 1 < resLen:
                     resLen = r - l + 1
                     res = [l, r]
-                
                 window[s[l]] -= 1
-                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                if c in countT and countT[c] > window[c]:
                     have -= 1
                 l += 1
         l, r = res
-        return s[l : r + 1] if resLen != float("inf") else ""
+        return s[l: r + 1] if resLen != float('inf') else ""
+
+
+
