2005c1571f167e1d416293ef77be7897f33c3207
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-17 11:38:50 -0400

    Time: 334 ms (5.88%), Space: 39 MB (60.51%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -2,10 +2,12 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pairs = [[p, s] for p, s in zip(position, speed)]
         res = []
-        pairs.sort(reverse = True)
+        pairs.sort(reverse=True)
+        # print(pairs)
         for p, s in pairs:
             res.append((target - p) / s)
             while len(res) > 1 and res[-1] <= res[-2]:
                 res.pop()
         return len(res)
-        
+            
+            
