bd3c70ed92df1219ed55948496c0996bd1621235
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-22 13:32:28 -0400

    Time: 10 ms (77.11%), Space: 27.8 MB (17.28%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -6,36 +6,33 @@ class Twitter:
         self.followMap = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append([-self.count, tweetId])       
-        self.count += 1 
+        self.tweetMap[userId].append([-self.count, tweetId])
+        self.count += 1        
 
     def getNewsFeed(self, userId: int) -> List[int]:
         res = []
-        minHeap = []
-
+        heap = []
         self.followMap[userId].add(userId)
-        for followeeId in self.followMap[userId]:
-            if followeeId in self.tweetMap:
-                index = len(self.tweetMap[followeeId]) - 1
-                count, tweetId = self.tweetMap[followeeId][index]
-                minHeap.append([count, tweetId, followeeId, index - 1])
-        heapq.heapify(minHeap)
-        while minHeap and len(res) < 10:
-            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+        for followerId in self.followMap[userId]:
+            if followerId in self.tweetMap:
+                index = len(self.tweetMap[followerId]) - 1
+                count, tweetId = self.tweetMap[followerId][index]
+                heap.append([count, tweetId, followerId, index - 1])
+        heapq.heapify(heap)
+        while heap and len(res) < 10:
+            count, tweetId, followerId, index = heapq.heappop(heap)
             res.append(tweetId)
             if index >= 0:
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+                count, tweetId = self.tweetMap[followerId][index]
+                heapq.heappush(heap, [count, tweetId, followerId, index - 1])
         return res
-
+        
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].add(followeeId)
-        
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.followMap[followerId]:
-            self.followMap[followerId].remove(followeeId)
+        self.followMap[followerId].discard(followeeId)
         
 
 
