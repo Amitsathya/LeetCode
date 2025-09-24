1fec9094a3321ca30c063d5ccdeadf8e1133046d
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-24 19:30:08 -0400

    Time: 12 ms (66.8%), Space: 27.7 MB (33.83%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -7,30 +7,27 @@ class Twitter:
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweetMap[userId].append([-self.count, tweetId])
-        self.count += 1       
+        self.count += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        res = []
-        heap = []
-        self.followMap[userId].add(userId)
+        res, heap = [], []
+        self.followMap[userId].add(userId)        
         for followerId in self.followMap[userId]:
             if followerId in self.tweetMap:
                 index = len(self.tweetMap[followerId]) - 1
-                count, tweetId = self.tweetMap[followerId][index]
-                heap.append([count, tweetId, followerId, index - 1])
+                count, tweet = self.tweetMap[followerId][index]
+                heap.append([count, tweet, followerId, index - 1])
         heapq.heapify(heap)
         while heap and len(res) < 10:
-            count, tweetId, followerId, index = heapq.heappop(heap)
-            res.append(tweetId)
+            count, tweet, followerId, index = heapq.heappop(heap)
+            res.append(tweet)
             if index >= 0:
-                count, tweetId = self.tweetMap[followerId][index]
-                heapq.heappush(heap,[count, tweetId, followerId, index - 1])
-        return res
-
+                count, tweet = self.tweetMap[followerId][index]
+                heapq.heappush(heap, [count, tweet, followerId, index - 1])
+        return res        
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.followMap[followerId].add(followeeId)
-        
+        self.followMap[followerId].add(followeeId)        
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].discard(followeeId)
