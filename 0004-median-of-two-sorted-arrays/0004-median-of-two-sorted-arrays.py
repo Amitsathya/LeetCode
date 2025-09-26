d38099e4af6f7ba4388c6ec5b00971aa04d32131
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-25 20:12:15 -0400

    Time: 0 ms (100%), Space: 18.3 MB (19.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -11,15 +11,18 @@ class Solution:
         while True:
             i = (l + r) // 2
             j = half - i - 2
+
             leftA = A[i] if i >= 0 else float('-inf')
             leftB = B[j] if j >= 0 else float('-inf')
-            rightA = A[i + 1] if (i + 1) < len(A) else float('inf')
-            rightB = B[j + 1] if (j + 1) < len(B) else float('inf')
+            rightA = A[i + 1] if i + 1 < len(A) else float('inf')
+            rightB = B[j + 1] if j + 1 < len(B) else float('inf')
+
             if leftA <= rightB and leftB <= rightA:
                 if total % 2:
                     return min(rightA, rightB)
-                return (max(leftA, leftB) + min(rightA, rightB)) / 2
-            elif leftA > rightB:
+                return (min(rightA, rightB) + max(leftA, leftB)) / 2
+            if leftA > rightB:
                 r = i - 1
             else:
                 l = i + 1
+        
