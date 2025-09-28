978a6dc22fa2d5b247f8f14481205a26776560be
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-09-28 09:29:16 -0400

    Time: 3 ms (81.23%), Space: 17.9 MB (44.62%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
-        rotten = set()
-        good = set()
-        q = deque()
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        q = collections.deque()
+        fresh = 0
+        time = 0
 
-        def turnIntoRotten(r, c):
-            if (r < 0 or r == ROWS or c < 0 or c == COLS or (r, c) in rotten or (r, c) not in good):
-                return
-            rotten.add((r, c))
-            q.append((r, c))
-            good.remove((r, c))
-
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh += 1
                 if grid[r][c] == 2:
-                    rotten.add((r, c))
                     q.append((r, c))
-                elif grid[r][c] == 1:
-                    good.add((r, c))
-        time = -1
-        while q:
-            for _ in range(len(q)):
-                r, c  = q.popleft()
+
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while fresh > 0 and q:
+            length = len(q)
+            for i in range(length):
+                r, c = q.popleft()
+
                 for dr, dc in directions:
-                    turnIntoRotten(r + dr, c + dc)
+                    row, col = r + dr, c + dc
+                    if (row in range(len(grid))
+                        and col in range(len(grid[0]))
+                        and grid[row][col] == 1
+                    ):
+                        grid[row][col] = 2
+                        q.append((row, col))
+                        fresh -= 1
             time += 1
-        return max(time, 0) if len(good) == 0 else -1
-
-
-        
+        return time if fresh == 0 else -1
