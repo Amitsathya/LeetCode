ceee1553465166a462b7123e3972d40b7f9f4b84
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-01 09:44:47 -0400

    Time: 7 ms (56%), Space: 21.8 MB (43.01%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,28 +1,30 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         ROWS, COLS = len(board), len(board[0])
         directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-        def capture(r, c):
+        def addCell(r, c):
             if (r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != 'O'):
                 return
             board[r][c] = "T"
             for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                capture(nr, nc)
+                addCell(r + dr, c + dc)
         
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == 'O' and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
-                    capture(r, c)
-
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = 'X'
+                    addCell(r, c)
         
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == "T":
-                    board[r][c] = 'O'
-                
+                if board[r][c] == 'O':
+                    board[r][c] = "X"
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'T':
+                    board[r][c] = "O"
+
+         
