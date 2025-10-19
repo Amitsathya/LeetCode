c70e5597ce9ae87c8bd45143e75bd70af1762e59
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-18 20:23:45 -0400

    Time: 7978 ms (9.2%), Space: 19.3 MB (41.5%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -2,10 +2,10 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
-
-    def addWord(self, word):
+    
+    def addWord(self, w):
         curr = self
-        for c in word:
+        for c in w:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
@@ -17,13 +17,13 @@ class Solution:
         for w in words:
             root.addWord(w)
         ROWS, COLS = len(board), len(board[0])
-        res, visit = set(), set()
+        visit, res = set(), set()
         def dfs(r, c, word, node):
             if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] not in node.children or (r, c) in visit:
                 return
             visit.add((r, c))
-            word += board[r][c]
             node = node.children[board[r][c]]
+            word += board[r][c]
             if node.endOfWord:
                 res.add(word)
             directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
@@ -35,4 +35,5 @@ class Solution:
             for c in range(COLS):
                 dfs(r, c, "", root)
         return list(res)
+                
         
