f3880258d63bb4a93d44d0f1a748ac247ec02ed1
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-20 09:43:30 -0400

    Time: 3 ms (76.37%), Space: 19.8 MB (12.14%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,31 +1,23 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        adj = { i : [] for i in range(numCourses)}
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = {i: [] for i in range(numCourses)}
         for c, p in prerequisites:
             adj[c].append(p)
-        visited, cycle = set(), set()
-        output = []
+        visit, cycle, res = set(), set(), []
         def dfs(c):
             if c in cycle:
                 return False
-            if c in visited:
+            if c in visit:
                 return True
             cycle.add(c)
             for nei in adj[c]:
                 if not dfs(nei):
                     return False
             cycle.remove(c)
-            visited.add(c)
-            output.append(c)
+            visit.add(c)
+            res.append(c)
             return True
         for i in range(numCourses):
             if not dfs(i):
                 return []
-        return output
-
-        
+        return res
