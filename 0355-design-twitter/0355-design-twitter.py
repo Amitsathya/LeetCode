44680db35fff2b29a23f85d18a1c5a1003070067
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-23 08:46:29 -0400

    Time: 19 ms (34.17%), Space: 27.8 MB (16.86%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -6,13 +6,12 @@ class Twitter:
         self.followMap = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append([-self.count, tweetId])       
-        self.count += 1 
+        self.tweetMap[userId].append([-self.count, tweetId])
+        self.count += 1        
 
     def getNewsFeed(self, userId: int) -> List[int]:
         res = []
         minHeap = []
-
         self.followMap[userId].add(userId)
         for followeeId in self.followMap[userId]:
             if followeeId in self.tweetMap:
@@ -27,7 +26,6 @@ class Twitter:
                 count, tweetId = self.tweetMap[followeeId][index]
                 heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
         return res
-
 
     def follow(self, followerId: int, followeeId: int) -> None:
         self.followMap[followerId].add(followeeId)
