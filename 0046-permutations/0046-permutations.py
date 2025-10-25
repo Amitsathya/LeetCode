86bad72f7807e4279dc5546a1b808feba74e1476
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-25 10:46:57 -0400

    Time: 1 ms (46.99%), Space: 18 MB (58.95%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        if len(nums) == 0:
-            return [[]]
+        n = len(nums)
+        ans, sol = [], []
 
-        perms = self.permute(nums[1:])
-        res = []
-        for p in perms:
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
-        return res
+        def backTrack():
+            if len(sol) == n:
+                ans.append(sol.copy())
+                return
+            for x in nums:
+                if x not in sol:
+                    sol.append(x)
+                    backTrack()
+                    sol.pop()
+        backTrack()
+        return ans
