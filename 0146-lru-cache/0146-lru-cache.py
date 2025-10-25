ed5c595defdac2d1d68b43e51640bac76b813670
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-10-25 14:08:47 -0400

    Time: 145 ms (48.04%), Space: 77.8 MB (91.64%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,42 +1,43 @@
 class Node:
     def __init__(self, key, val):
-        self.val = val
         self.key = key
-        self.prev = self.next = None
+        self.val = val
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.cap = capacity
         self.cache = {}
-        self.right, self.left = Node(0, 0), Node(0, 0)
-        self.right.prev, self.left.next = self.left, self.right
-    
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
     def insert(self, node):
         prev, curr = self.right.prev, self.right
         prev.next = curr.prev = node
-        node.prev, node.next = prev, curr
-    
-    def remove(self, node):
+        node.next, node.prev = curr, prev
+
+    def delete(self, node):
         prev, curr = node.prev, node.next
         prev.next, curr.prev = curr, prev
 
     def get(self, key: int) -> int:
         if key in self.cache:
-            self.remove(self.cache[key])
+            self.delete(self.cache[key])
             self.insert(self.cache[key])
             return self.cache[key].val
-        return -1      
+        return -1       
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+            self.delete(self.cache[key])
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
 
         if len(self.cache) > self.cap:
             node = self.left.next
-            self.remove(node)
+            self.delete(node)
             del self.cache[node.key]
 
 # Your LRUCache object will be instantiated and called as such:
