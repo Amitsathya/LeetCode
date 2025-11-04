c584f1537129e5e12358213b7e795d1cf1bcafa4
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2025-11-03 19:17:00 -0500

    Time: 7 ms (42.78%), Space: 18.8 MB (41.83%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {} #(index, buying) val: profit
+        dp = {} #key: index, buy/sell value: profit
 
         def dfs(i, buying):
-            if i >= len(prices): 
+            if i >= len(prices):
                 return 0
             if (i, buying) in dp:
                 return dp[(i, buying)]
@@ -11,9 +11,9 @@ class Solution:
             cooldown = dfs(i + 1, buying)
             if buying:
                 buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
+                dp[(i, buying)] = max(cooldown, buy)
             else:
                 sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
+                dp[(i, buying)] = max(cooldown, sell)
             return dp[(i, buying)]
         return dfs(0, True)
