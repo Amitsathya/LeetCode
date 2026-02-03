4821d8ca63401672b2d41a01baf8ab77435cfb43
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-03 10:36:44 -0500

    Time: 79 ms (87.34%), Space: 19.9 MB (8.5%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,8 +6,9 @@ class Solution:
         for r in range(len(s)):
             count[s[r]] = count.get(s[r], 0) + 1
             maxF = max(maxF, count[s[r]])
-            while r - l + 1 - maxF > k:
+            if r - l + 1 - maxF > k:
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1)
         return res
+            
