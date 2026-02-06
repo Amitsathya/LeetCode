75226f3cfad9c5a3fc17a1673b66e5b91adcae89
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-05 20:47:08 -0500

    Time: 21 ms (14.15%), Space: 23.1 MB (60.12%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -10,9 +10,11 @@ class Solution:
         def dfs(root):
             if not root:
                 return 0
-            leftMax = max(dfs(root.left), 0)
-            rightMax = max(dfs(root.right), 0)
+            
+            leftMax = max(0, dfs(root.left))
+            rightMax = max(0, dfs(root.right))
             res[0] = max(res[0], leftMax + rightMax + root.val)
             return root.val + max(leftMax, rightMax)
         dfs(root)
         return res[0]
+        
