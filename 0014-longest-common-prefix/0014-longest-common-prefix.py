0cb3295e004a649230f79d2b16178ce078da9a05
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-18 08:44:38 -0500

    Time: 4 ms (9.38%), Space: 19.2 MB (70.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,61 +1,11 @@
-# class TrieNode:
-#     def __init__(self):
-#         self.children = {}
-#         self.endOfWord = False
-
-# class Solution:
-#     def __init__(self):
-#         self.root = TrieNode()
-
-#     def longestCommonPrefix(self, strs: List[str]) -> str:
-#         res = []
-#         for s in strs:
-#             curr = self.root
-#             for c in s:
-#                 if c not in curr.children:
-#                     curr.children[c] = TrieNode()
-#                 curr = curr.children[c]
-#             curr.endOfWord = True
-        
-#         def dfs(root):
-#             children = root.children.values()
-#             if not children and len(children) > 1:
-#                 return "".join(res)
-#             res.append(children[0])
-#             dfs(children[0])
-#         return dfs(self.root)
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endOfWord = False
-
 class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if not strs:
             return ""
 
-        # Build trie
-        for s in strs:
-            curr = self.root
-            for c in s:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.endOfWord = True
-
-        # Traverse while only one child and not end of word
-        res = []
-        curr = self.root
-
-        while curr and len(curr.children) == 1 and not curr.endOfWord:
-            c, next_node = next(iter(curr.children.items()))
-            res.append(c)
-            curr = next_node
-
-        return "".join(res)
-        
-
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for s in strs[1:]:
+                if len(s) == i or s[i] != c:
+                    return s[:i]
+        return strs[0]
