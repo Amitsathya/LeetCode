3ee553bf0d99884670b88a8ecde64f4d06b34d65
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-18 20:48:07 -0500

    Time: 263 ms (63.62%), Space: 17.8 MB (79.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def isPali(l, r):
+        res = ""
+        def isValid(l, r):
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 l, r = l - 1, r + 1
-            return s[l + 1: r]
-        res = ""
+            return s[l + 1 : r]
         for i in range(len(s)):
-            temp1 = isPali(i, i)
-            temp2 = isPali(i, i + 1)
+            temp1 = isValid(i, i)
+            temp2 = isValid(i, i + 1)
             if len(res) < len(temp1):
                 res = temp1
             if len(res) < len(temp2):
