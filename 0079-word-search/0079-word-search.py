9a067f54df016d56193162a68a7e9045e780a7de
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-22 09:38:23 -0500

    Time: 5681 ms (10.21%), Space: 19.7 MB (6.85%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,19 +5,17 @@ class Solution:
         def dfs(i, r, c):
             if i == len(word):
                 return True
-            if r < 0 or r >= ROWS or c < 0 or c >= COLS or word[i] != board[r][c] or (r, c) in visited:
-                return
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or board[r][c] != word[i] or (r, c) in visited:
+                return False
             visited.add((r, c))
             directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
             for dr, dc in directions:
-                row, col = r + dr, dc + c
+                row, col = dr + r, dc + c
                 if dfs(i + 1, row, col):
                     return True
             visited.remove((r, c))
-            return False
         for r in range(ROWS):
             for c in range(COLS):
                 if dfs(0, r, c):
                     return True
         return False
-            
