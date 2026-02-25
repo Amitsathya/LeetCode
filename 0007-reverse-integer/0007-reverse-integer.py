2f3658696f85b60507174e58717d36174a785651
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-24 22:11:32 -0500

    Time: 43 ms (65.95%), Space: 19.3 MB (61.09%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,22 +1,18 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        rev = 0
-        
-        while x != 0:
+        res = 0
+        while x!=0:
             digit = x % 10
-            
-            # Python specific fix for negative numbers
+
             if x < 0 and digit > 0:
                 digit -= 10
-            
-            x = (x - digit) // 10
-            
-            # Overflow check
-            if rev > 214748364 or (rev == 214748364 and digit > 7):
+
+            x = (x-digit) // 10
+            # x = x//10
+            if res > 214748364 or (res == 214748364 and digit > 7):
                 return 0
-            if rev < -214748364 or (rev == -214748364 and digit < -8):
+            if res < -214748364 or (res == -214748364 and digit <-8):
                 return 0
             
-            rev = rev * 10 + digit
-        
-        return rev
+            res = res * 10 + digit
+        return res 
