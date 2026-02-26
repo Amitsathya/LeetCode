6edee6fc8148df1366dedadb9f6a57dcf2276b5a
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-26 16:52:09 -0500

    Time: 51 ms (17.95%), Space: 19.6 MB (84.32%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,27 +1,23 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        atlanticVisited, pacificVisited = set(), set()
         ROWS, COLS = len(heights), len(heights[0])
-        res = []
-
+        pacific_visited, atlantic_visited = set(), set()
         def dfs(r, c, visited):
             visited.add((r, c))
             directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
             for dr, dc in directions:
-                nr, nc = dr + r, dc + c
-                if 0 <= nr < ROWS and 0 <= nc < COLS and (nr, nc) not in visited and heights[r][c] <= heights[nr][nc]:
-                    dfs(nr, nc, visited)
-        
+                row, col = dr + r, dc + c
+                if 0 <= row < ROWS and 0 <= col < COLS and (row, col) not in visited and heights[row][col] >= heights[r][c]:
+                    dfs(row, col, visited)
         for i in range(ROWS):
-            dfs(i, 0, pacificVisited)
-            dfs(i, COLS - 1, atlanticVisited)
-
-        for j in range(COLS):
-            dfs(0, j, pacificVisited)
-            dfs(ROWS - 1, j, atlanticVisited)
-
+            dfs(i, 0, pacific_visited)
+            dfs(i, COLS - 1, atlantic_visited)
+        for i in range(COLS):
+            dfs(0, i, pacific_visited)
+            dfs(ROWS - 1, i, atlantic_visited)
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r, c) in pacificVisited and (r, c) in atlanticVisited:
+                if (r, c) in pacific_visited and (r, c) in atlantic_visited:
                     res.append((r, c))
         return res
