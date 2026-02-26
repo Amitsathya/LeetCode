57f364ee9f492b0fb1af3d3dba82ba6f6a70f384
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-02-25 23:10:09 -0500

    Time: 108 ms (61.52%), Space: 61.5 MB (42.88%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,13 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        def dfs(i, curr):
+            if len(curr) == k:
+                res.append(curr.copy())
+                return
+            for j in range(i, n + 1):
+                curr.append(j)
+                dfs(j + 1, curr)
+                curr.pop()
+        dfs(1, [])
+        return res
