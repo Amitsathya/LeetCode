8705ce2bfafef6ff72659a489e069c9f57d0ac7b
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-03-08 11:17:19 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,10 +5,9 @@ class Solution(object):
         :rtype: int
         """
         ROWS, COLS = len(grid), len(grid[0])
-        q = deque()
-        directions = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+        directions = [[-1, 0], [0, -1], [0, 1],[1, 0]]
         fresh = 0
-
+        q = deque()
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
@@ -22,8 +21,8 @@ class Solution(object):
                 for dr, dc in directions:
                     nr, nc = dr + r, dc + c
                     if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 1:
-                        q.append((nr, nc))
                         grid[nr][nc] = 2
                         fresh -= 1
+                        q.append((nr, nc))
             time += 1
         return time if fresh == 0 else -1
