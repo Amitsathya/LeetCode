1e83fae1e9ca832b21b4c498129289a443d1b935
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-03-08 11:33:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,28 +4,8 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         ROWS, COLS = len(board), len(board[0])
-        q = deque()
-        visited = set()
-        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-
-        def addCell(r, c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or board[r][c] != 'O':
-                return
-            board[r][c] = 'T'
-            for dr, dc in directions:
-                addCell(dr + r, dc + c)
-
         for r in range(ROWS):
             for c in range(COLS):
-                if board[r][c] == 'O' and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
-                    addCell(r, c)
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == 'O':
-                    board[r][c] = 'X'
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == 'T':
-                    board[r][c] = 'O'
+                if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
+                    board[r][c] == "T"
+        print(board)
