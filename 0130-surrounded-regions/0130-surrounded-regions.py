85bec8402835017d64b8c4459982c03a7d102ab7
Amit Sathya <40379813+Amitsathya@users.noreply.github.com>
2026-03-08 11:47:44 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -4,8 +4,23 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         ROWS, COLS = len(board), len(board[0])
+        directions = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+        def dfs(r, c):
+            if 0 > r or r >= ROWS or c < 0 or c >= COLS or board[r][c] != 'O':
+                return
+            board[r][c] = 'T'
+            for dr, dc in directions:
+                dfs(dr +r, dc + c)
+    
         for r in range(ROWS):
             for c in range(COLS):
                 if board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1]):
-                    board[r][c] == "T"
-        print(board)
+                    dfs(r, c)
+        # print(board)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                if board[r][c] == "T":
+                    board[r][c] = "O"
